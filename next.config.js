eabf822b91aff4d3a936074f9be49d27cefafc82
DoLT <dolt@runsytem.net>
2021-10-21 19:24:46 +0700

    fix cors

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  env: {
-    PUBLIC_URL:
-      process.env.NODE_ENV === "production"
-        ? "https://test-next-graphql.netlify.app"
-        : "http://localhost:3000",
-  },
 };
