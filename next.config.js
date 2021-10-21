d1c3f9da3055e2381df30cb9a4f3acef7e62511f
DoanTanLoc-2203 <doanlocpro23@gmail.com>
2021-10-21 19:12:28 +0700

    fix config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    PUBLIC_URL:
+      process.env.NODE_ENV === "production"
+        ? "https://test-next-graphql.netlify.app"
+        : "http://localhost:3000",
+  },
+};
